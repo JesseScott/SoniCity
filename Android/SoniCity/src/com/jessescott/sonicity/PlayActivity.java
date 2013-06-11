665f5e06cf5669851e191c0cd487df734f34abda
Jesse Scott <jesse@memelab.ca>
2013-06-11 09:55:11 -0400

    Added GPSListener

--- Android/SoniCity/src/com/jessescott/sonicity/PlayActivity.java
+++ Android/SoniCity/src/com/jessescott/sonicity/PlayActivity.java
@@ -9,11 +9,20 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationListener;
+import android.location.GpsStatus.Listener;
+import android.location.GpsStatus.NmeaListener;
 
 import org.puredata.android.io.AudioParameters;
 import org.puredata.android.service.PdService;
@@ -23,13 +32,24 @@ import org.puredata.core.PdListener;
 import org.puredata.core.utils.IoUtils;
 
 
-/* CLASS */
+/* PLAYACTIVITY CLASS */
 public class PlayActivity extends Activity {
 	
 	// GLOBALS
 	private static final String TAG = "SoniCity";
 	private PdUiDispatcher dispatcher;
 	private PdService pdService = null;
+	LocationManager locationManager;
+	MyLocationListener locationListener;
+	Paint paint = new Paint();
+	
+	float currentLatitude  	= 0;
+	float currentLongitude 	= 0;
+	float currentAltitude 	= 0;
+	float currentAccuracy  	= 0;
+	float currentSpeed 		= 0;
+	float currentBearing 	= 0;
+	String currentProvider 	= "";
 	
 	/* LIBPD */
 	
@@ -87,11 +107,6 @@ public class PlayActivity extends Activity {
 		File patchFile = new File(dir, "sonicity.pd");
 		PdBase.openPatch(patchFile.getAbsolutePath());
 	}
-	// Gui
-	private void initGui() {
-		setContentView(R.layout.play_layout);
-
-	}
 	
 	/* SEND DATA */
 
@@ -121,15 +136,35 @@ public class PlayActivity extends Activity {
 		}, PhoneStateListener.LISTEN_CALL_STATE);
 	}
 	
+	/* UI */
+	 protected void onDraw(Canvas canvas) {
+	 	paint.setColor(Color.BLUE);
+	 	paint.setTextSize(36);
+	 	canvas.drawText(String.valueOf("OUR LAT IS: " + currentLatitude),  100, 100, paint);
+	 	canvas.drawText(String.valueOf("OUR LON IS: " + currentLongitude), 100, 200, paint);
+	 	canvas.drawText(String.valueOf("OUR ALT IS: " + currentAltitude),  100, 300, paint);
+	 	canvas.drawText(String.valueOf("OUR ACC IS: " + currentAccuracy),  100, 400, paint);
+	 	canvas.drawText(String.valueOf("OUR SPE IS: " + currentSpeed),     100, 500, paint);
+	 	canvas.drawText(String.valueOf("OUR BEA IS: " + currentBearing),   100, 600, paint);
+	 	canvas.drawText(String.valueOf("OUR PRO IS: " + currentProvider),  100, 700, paint);
+	 	//invalidate();
+	}
+	
 	/* LIFECYCLE */
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Log.v(TAG, " - Starting The Play Screen - ");
+		
+		// GPS
+		locationListener = new MyLocationListener(this);
 
 		// UI
-		initGui();
+		setContentView(R.layout.play_layout);
+		//setContentView(locationListener);
+		
+		// Services
 		initSystemServices();
 		bindService(new Intent(this, PdService.class), pdConnection, BIND_AUTO_CREATE);
 		
@@ -144,7 +179,9 @@ public class PlayActivity extends Activity {
 	@Override
 	protected void onResume() {
 		super.onResume();
-		
+		// GPS
+		locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 5, locationListener);
 	}
 	
 	@Override
@@ -153,4 +190,41 @@ public class PlayActivity extends Activity {
 		unbindService(pdConnection);
 	}
 
-}
+
+
+/* PLAYACTIVITY CLASS */
+class MyLocationListener implements LocationListener {
+
+  public MyLocationListener(Context context) {
+		super();
+  }
+
+  // Define all LocationListener methods
+  public void onLocationChanged(Location location) {
+    // Save new GPS data
+    currentLatitude  = (float)location.getLatitude();
+    currentLongitude = (float)location.getLongitude();
+    currentAltitude = (float)location.getAltitude();
+    currentAccuracy  = (float)location.getAccuracy();
+	currentSpeed = (float)location.getSpeed();
+	currentBearing = (float)location.getBearing();
+    currentProvider  = location.getProvider();
+  }
+
+  public void onProviderDisabled (String provider) { 
+    currentProvider = "";
+  }
+
+  public void onProviderEnabled (String provider) { 
+    currentProvider = provider;
+  }
+
+  public void onStatusChanged (String provider, int status, Bundle extras) {
+    // Nothing yet...
+  }
+  
+
+  
+} /* */
+
+} /*  */
