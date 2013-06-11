ea09e292a3bc92588895a31b39a92b2f0361958b
Jesse Scott <jesse@memelab.ca>
2013-06-11 08:44:42 -0400

    Removed Unnecessary Imports

--- Android/SoniCity/src/com/jessescott/sonicity/AboutActivity.java
+++ Android/SoniCity/src/com/jessescott/sonicity/AboutActivity.java
@@ -1,12 +1,11 @@
 package com.jessescott.sonicity;
 
+/* IMPORTS */
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 
+/* CLASS */
 public class AboutActivity extends Activity {
 	
 	// GLOBALS
