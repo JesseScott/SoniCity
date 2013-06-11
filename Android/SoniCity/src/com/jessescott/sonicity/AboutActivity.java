378605f45bb3e5622d1b20e3fe821fc5166f99dc
Jesse Scott <jesse@memelab.ca>
2013-06-11 08:57:25 -0400

    About Screen Hyperlinks Pt 1

--- Android/SoniCity/src/com/jessescott/sonicity/AboutActivity.java
+++ Android/SoniCity/src/com/jessescott/sonicity/AboutActivity.java
@@ -2,14 +2,19 @@ package com.jessescott.sonicity;
 
 /* IMPORTS */
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 /* CLASS */
 public class AboutActivity extends Activity {
 	
 	// GLOBALS
 	private static final String TAG = "SoniCity";
+	private TextView textView_Website;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,18 @@ public class AboutActivity extends Activity {
 		// UI
 		setContentView(R.layout.about_layout);
 		Log.v(TAG, " - Starting The About Screen - ");
+		
+		// TextView
+		textView_Website = (TextView) findViewById(R.id.textView_Website);
+		textView_Website.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Log.v(TAG, "Clicked The Website Link!");
+				Uri uri = Uri.parse("http://www.jesses.co.tt"); 
+				Intent intent = new Intent(Intent.ACTION_VIEW, uri); 
+				startActivity(intent);	
+			}
+		});
 		
 	}
 	
