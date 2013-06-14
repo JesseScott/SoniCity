c8e66723b5de9e642d05083af8a5ac0222b4d8cc
Jesse Scott <jesse@memelab.ca>
2013-06-14 18:20:09 -0400

    Processing Test Pt 1

---
+++ Android/P5Test/src/com/example/p5test/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.p5test;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import processing.core.*;
+
+public class MainActivity extends PApplet {
+	/*
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	*/
+	
+	public void setup() {
+		background(255, 0, 0);
+		
+	}
+	
+	public void draw() {
+		
+		
+	}
+	
+	
+}
